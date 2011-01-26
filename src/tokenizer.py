456988eb2483e0e7b0a65dbe8a9100e9c706291b
Peter F. Schulam <pschulam@gmail.com>
2011-01-26 16:17:36 -0500

    Removed tokenizer class, now simply a method in a module named tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -6,27 +6,15 @@ Created on Jan 20, 2011
 
 from nltk import sent_tokenize, word_tokenize
 
-class Tokenizer(object):
+def tokenize(filename):
     '''
-    Does all of the steps necessary before tagging and chunking.
-    Mainly tokenizes.
+    Split the text in the given file into a list of lists of words.
+    Each nested list should be one sentence in the document.
     '''
-
-    def __init__(self):
-        '''
-        Constructor. Doesn't do anything.
-        '''
-        pass
-
-    def tokenize(self, filename):
-        '''
-        Split the text in the given file into a list of lists of words.
-        Each nested list should be one sentence in the document.
-        '''
-        fd = open(filename, 'r')
-        doc = fd.read()
-        fd.close()
-        sents = sent_tokenize(doc)
-        sents = [word_tokenize(sent) for sent in sents]
-        return sents
+    fd = open(filename, 'r')
+    doc = fd.read()
+    fd.close()
+    sents = sent_tokenize(doc)
+    sents = [word_tokenize(sent) for sent in sents]
+    return sents
 
